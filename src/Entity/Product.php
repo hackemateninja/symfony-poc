6293cf681f7bc5b631bdd3c8ef600db298031613
Herman <nitrowitty@gmail.com>
2025-04-28 03:11:34 -0600

    fix product entity

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -99,8 +99,13 @@
 		#[ORM\PrePersist]
 		public function setCreatedAtValue(): void
 		{
+			$now = new \DateTimeImmutable();
+			
 			if ($this->createdAt === null) {
-				$this->createdAt = new \DateTimeImmutable();
+				$this->createdAt = $now;
+			}
+			if ($this->updatedAt === null) {
+				$this->updatedAt = $now;
 			}
 		}
 		
