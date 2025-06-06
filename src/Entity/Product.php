69061e2abd570eefc78e90505261c7d7c26a4c66
Herman <nitrowitty@gmail.com>
2025-06-06 05:13:23 -0600

    many to many

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,14 +2,16 @@
 	
 	namespace App\Entity;
 	
-	use App\Repository\ProductRepository;
+	use App\Repository\categoryRepository;
+  use Doctrine\Common\Collections\ArrayCollection;
+  use Doctrine\Common\Collections\Collection;
 	use Doctrine\DBAL\Types\Types;
 	use Doctrine\ORM\Mapping as ORM;
 	use Symfony\Component\Validator\Constraints as Assert;
 	
-	#[ORM\Entity(repositoryClass: ProductRepository::class)]
+	#[ORM\Entity(repositoryClass: categoryRepository::class)]
 	#[ORM\HasLifecycleCallbacks]
-	class Product
+	class category
 	{
 		#[ORM\Id]
 		#[ORM\GeneratedValue]
@@ -35,7 +37,19 @@
 		
 		#[ORM\Column]
 		private ?\DateTimeImmutable $updatedAt = null;
-		
+
+    /**
+     * @var Collection<int, Category>
+     */
+    #[ORM\ManyToMany(targetEntity: Category::class, mappedBy: 'categorys')]
+    #[ORM\JoinTable(name: 'user_groups')]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
 		
 		public function getId(): ?int
 		{
@@ -119,5 +133,32 @@
 		public function setUpdatedAtValue(): void
 		{
 			$this->updatedAt = new \DateTimeImmutable();
+		}
+
+		/**
+		* @return Collection<int, Category>
+		*/
+		public function getCategories(): Collection
+		{
+		  return $this->categories;
+		}
+		
+		public function addCategory(Category $category): static
+		{
+		  if (!$this->categories->contains($category)) {
+		      $this->categories->add($category);
+		      $category->addProduct($this);
+		  }
+		
+		  return $this;
+		}
+		
+		public function removeCategory(Category $category): static
+		{
+		  if ($this->categories->removeElement($category)) {
+		      $category->removeProduct($this);
+		  }
+		
+		  return $this;
 		}
 	}
